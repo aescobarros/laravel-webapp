d025c93e01255f5911e037e5cb3c9eb34e2089de
aescobarros <114923210+aescobarros@users.noreply.github.com>
2025-09-21 15:02:20 +0200

    auth and permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+
+        Artisan::call('db:seed', ['--class' => RolesAndPermissionsSeeder::class]);
+
+        User::factory(35)->create()->each(function ($user) {
+            $user->assignRole('user');
+        });
 
         User::factory()->create([
             'name' => 'Test User',
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        // first include all the normal setUp operations
+        parent::setUp();
+
+        $this->artisan('db:seed', ['--class' => RolesAndPermissionsSeeder::class]);
+        $this->app->make(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+    }
 }
